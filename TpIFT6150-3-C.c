b922714993f484b2ef934676129ef17cbfaa0b20
Nicolas Hurtubise <316k@legtux.org>
2016-11-09 10:40:34 -0500

    Closer to the final result

--- TpIFT6150-3-C.c
+++ TpIFT6150-3-C.c
@@ -31,14 +31,13 @@ float difference_squared(float** M1, float** M2, int length, int width);
 void landweber(float**, float**, float**, float**, float**, float**, float**, float, int, int, int);
 void mult_pi_fct(float** f, int length, int width);
 
-
 int main(int argc,char** argv)
 {
-    int nb_iterations;
+    int nb_iterations = 25;
     int i,j,k,l;
     int length,width;
-    float var;
-    int size_filter;
+    float var = 20;
+    int size_filter = 5;
     int nbLevels = 3;
 
     float** image_r; /* image d'entree */
@@ -55,13 +54,13 @@ int main(int argc,char** argv)
 
     
     printf("Entrez la taille du filtre passe bas : ");
-    scanf("%d",&size_filter);	
+    //scanf("%d",&size_filter);	
 
     printf("Entrez la variance du bruit : ");
-    scanf("%f",&var);
+    //scanf("%f",&var);
  
     printf("Entrez le nombre d'itérations (LANDWEBER): ");
-    scanf("%d",&nb_iterations);
+    //scanf("%d",&nb_iterations);
 
     /* ouvrir l'image d'entree */
     /* lire l'image d'entree */	
@@ -91,22 +90,22 @@ int main(int argc,char** argv)
 
 
     /* ajouter du flou et du bruit a l'image d'entree (add_gaussian_noise) */
- 	    
+    
     /*Initialisation a zero de toutes les matrices */
     for(i=0;i<length;i++)
         for(j=0;j<width;j++) {
-          
+            
             if((i < size_filter/2.0 || i >= length - size_filter/2.0)  &&
                (j < size_filter/2.0 || j >= width - size_filter/2.0)) {
                 filter_r[i][j] = 1.0/(size_filter * size_filter);
             } else {
                 filter_r[i][j] = 0.0;
             }
-
+            
             filter_i[i][j] = 0.0;
             f_i[i][j] = 0.0;
         }
-
+    
     /* Ajouter du flou a l'image d'entree : g = image + flou */
     FFTDD(image_r, image_i, length, width);
     FFTDD(filter_r, filter_i, length, width);
@@ -127,9 +126,15 @@ int main(int argc,char** argv)
 
     // Configuration initiale
     /* 1er etape : deconvolution avec 'nb_iterations' de LANDWEBER */
+
+    for(i=0;i<length;i++)
+        for(j=0;j<width;j++) {
+            f[i][j] = g_r[i][j];
+        }
+    
     landweber(f, f_i, g_r, g_i, filter_r, filter_i,
               image_r, 1, nb_iterations, length, width);
-    mult_pi_fct(f, length, width);
+    // mult_pi_fct(f, length, width);
 
     /* 2e etape : Filtrage dans le domaine des ondelettes */
     char continuer = 1;
@@ -137,56 +142,82 @@ int main(int argc,char** argv)
 
     while(continuer) {
 
+        isnr = 10 * log10(difference_squared(image_r, g_r, length, width) /
+                          difference_squared(image_r, f,   length, width));
+
+        printf("%02d - ISNR : %lf\n", k, isnr);
+        
         char filename[50];
         sprintf(filename, "iter-%02d", k);
         SaveImagePgm(filename, f, length, width);
-            
+        
         for(i=0;i<length;i++)
             for(j=0;j<width;j++) {
                 f_prev[i][j] = f[i][j];
             }
-        
+
+        // FIXME
         landweber(f, f_i, g_r, g_i, filter_r, filter_i,
                   image_r, 1, 1, length, width);
+
+        if(k == 5) {
+            mult_pi_fct(f, length, width);
+            mult_pi_fct(f_i, length, width);
+            mult_pi_fct(g_r, length, width);
+            mult_pi_fct(g_i, length, width);
+            mult_pi_fct(filter_r, length, width);
+            mult_pi_fct(filter_i, length, width);
+            mult_pi_fct(image_r, length, width);
+
+            SaveImagePgm("1f", f, length, width);
+            SaveImagePgm("1f_i", f_i, length, width);
+            SaveImagePgm("1g_r", g_r, length, width);
+            SaveImagePgm("1g_i", g_i, length, width);
+            SaveImagePgm("1filter_r", filter_r, length, width);
+            SaveImagePgm("1filter_i", filter_i, length, width);
+            SaveImagePgm("1image_r", image_r, length, width);
+
+            return EXIT_SUCCESS;
+        }
         
         haar2D_complete(f, haar, nbLevels, length, width);
-        
+
         for(i=0;i<length;i++)
             for(j=0;j<width;j++) {
                 if(i < size_image_moyenne && j < size_image_moyenne)
                     continue;
 
-                haar[i][j] = fmax(0, SQUARE(haar[i][j]) - 3 * var) / haar[i][j];
+                if(haar[i][j] != 0)
+                    haar[i][j] = fmax(0, SQUARE(haar[i][j]) - 3 * var) / haar[i][j];
             }
 
         // Transformation inverse de Haar dans `f`
         ihaar2D_complete(haar, f, nbLevels, length, width);
 
-        mult_pi_fct(f, length, width);
-
-        // Calculer le ISNR
+        // Calculer le ISNR        
         isnr = 10 * log10(difference_squared(image_r, g_r, length, width) /
                           difference_squared(image_r, f,   length, width));
 
         printf("%02d - ISNR : %lf\n", k, isnr);
-
-        // Condition d'arrêt
-        continuer = (difference_squared(f, f_prev, length, width) /
-                     difference_squared(f_prev, zeros, length, width)) > 0.00001;
         
-        printf("Condition d'arrêt : %f / %f > 10⁻⁵ = %d\n",
-               difference_squared(f, f_prev, length, width),
-               difference_squared(f_prev, zeros, length, width),
-               continuer);
+        // Condition d'arrêt
+        float a = difference_squared(f, f_prev, length, width);
+
+        float b = difference_squared(f_prev, zeros, length, width);
+
+        continuer = (a / b) > 0.00001;
+
+        
+        printf("Condition d'arrêt : %f / %f > 10⁻⁵ = %d\n", a, b, continuer);
         k++;
     }
 
-    
+    mult_pi_fct(f, length, width);
     /* Sauvegarde des matrices sous forme d'image pgm */
     SaveImagePgm(NAME_IMG_OUT3, f, length, width);
     
     /* Liberation memoire pour les matrices */
-
+    
     /*retour sans probleme*/ 
     printf("\n C'est fini ... \n\n\n");
     return 0; 	 
@@ -194,8 +225,7 @@ int main(int argc,char** argv)
 
 
 void AddMatrix(float** matRout,float** matIout,float** mat1Rin,float** mat1Iin,
-               float** mat2Rin,float** mat2Iin,int lgth,int wdth)
-{
+               float** mat2Rin,float** mat2Iin,int lgth,int wdth) {
     int i,j;
  
     for(i=0;i<lgth;i++)
@@ -232,11 +262,6 @@ void landweber(float** f, float** f_i, // Image restaurée
     float** calcul2_i = fmatrix_allocate_2d(length, width);
 
     int i,j;
-    
-    for(i=0;i<length;i++)
-        for(j=0;j<width;j++) {
-            f[i][j] = g_r[i][j];
-        }
 
     for(i=0; i<nb_iterations; i++) {
         
@@ -269,8 +294,6 @@ void landweber(float** f, float** f_i, // Image restaurée
         
         AddMatrix(f, f_i, calcul1_r, calcul1_i, f, f_i, length, width);
     }
-
-    // mult_pi_fct(f, length, width);
 }
 
 /**
